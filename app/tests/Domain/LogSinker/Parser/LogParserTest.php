a08c5539d371116c236a4b16131ff0c0ee47782b
Pazniak Vadzim <vadzim.pazniak@innowise.com>
2025-05-05 11:28:51 +0300

    Add unit tests

---
+++ app/tests/Domain/LogSinker/Parser/LogParserTest.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Domain\LogSinker\Parser;
+
+use App\Domain\LogSinker\LogEntry;
+use App\Domain\LogSinker\Parser\LogParser;
+use App\Domain\LogSinker\Parser\ParserStrategyInterface;
+use App\Domain\LogSinker\Reader\ReaderInterface;
+use Carbon\Carbon;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
+final class LogParserTest extends TestCase
+{
+    public function testSuccessfulParse(): void
+    {
+        $logLine = 'USER-SERVICE - - [18/Aug/2018:10:33:59 +0000] "POST /users HTTP/1.1" 201';
+        $carbon = new Carbon('18/Aug/2018:10:33:59 +0000');
+
+        $parsedEntry = new LogEntry(
+            'USER-SERVICE',
+            \DateTimeImmutable::createFromMutable($carbon),
+            'POST /users HTTP/1.1',
+            201
+        );
+
+        $reader = $this->createMock(ReaderInterface::class);
+        $reader->method('read')->willReturn([$logLine]);
+
+        $strategy = $this->createMock(ParserStrategyInterface::class);
+        $strategy->expects($this->once())
+            ->method('parseEntry')
+            ->with($logLine)
+            ->willReturn($parsedEntry)
+        ;
+
+        $logger = $this->createMock(LoggerInterface::class);
+        $logger->expects($this->never())->method('debug');
+
+        $parser = new LogParser($strategy, $logger);
+
+        $result = iterator_to_array($parser->parseFrom($reader));
+
+        $this->assertCount(1, $result);
+        $this->assertSame($parsedEntry, $result[0]);
+    }
+
+    public function testParseReturnsNullLogsDebug(): void
+    {
+        $logLine = 'INVALID LOG LINE';
+
+        $reader = $this->createMock(ReaderInterface::class);
+        $reader->method('read')->willReturn([$logLine]);
+
+        $strategy = $this->createMock(ParserStrategyInterface::class);
+        $strategy->expects($this->once())
+            ->method('parseEntry')
+            ->with($logLine)
+            ->willReturn(null)
+        ;
+
+        $logger = $this->createMock(LoggerInterface::class);
+        $logger->expects($this->atLeastOnce())->method('debug');
+
+        $parser = new LogParser($strategy, $logger);
+
+        $result = iterator_to_array($parser->parseFrom($reader));
+
+        $this->assertCount(0, $result);
+    }
+}
---
+++ app/tests/Domain/LogSinker/Reader/FileReaderTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Domain\LogSinker\Reader;
+
+use App\Domain\LogSinker\Exception\FileNotReadableException;
+use App\Domain\LogSinker\Reader\FileReader;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
+final class FileReaderTest extends TestCase
+{
+    private string $tempFile;
+
+    protected function tearDown(): void
+    {
+        if (isset($this->tempFile) && file_exists($this->tempFile)) {
+            unlink($this->tempFile);
+        }
+    }
+
+    public function testSuccessfulRead(): void
+    {
+        $lines = ['First line'.PHP_EOL, 'Second line'.PHP_EOL, 'Third line'.PHP_EOL];
+
+        $this->tempFile = tempnam(sys_get_temp_dir(), 'log_');
+        file_put_contents($this->tempFile, implode('', $lines));
+
+        $reader = new FileReader($this->tempFile);
+        $result = iterator_to_array($reader->read());
+
+        $this->assertSame($lines, $result);
+    }
+
+    public function testThrowsIfFileNotReadable(): void
+    {
+        $this->tempFile = tempnam(sys_get_temp_dir(), 'log_');
+        chmod($this->tempFile, 0000);
+
+        $reader = new FileReader($this->tempFile);
+
+        $this->expectException(FileNotReadableException::class);
+        iterator_to_array($reader->read());
+    }
+}
